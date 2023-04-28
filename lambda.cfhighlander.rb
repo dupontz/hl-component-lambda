d36ab91f63dc6a43131596af207622b86a8d5b23
dupontz <gustavo.dupont@gmail.com>
2023-04-28 15:00:00 -0700

    adding alb target group feature

--- lambda.cfhighlander.rb
+++ lambda.cfhighlander.rb
@@ -10,9 +10,34 @@ CfhighlanderTemplate do
     end
   end if defined? functions
 
+  DependsOn 'lib-iam'
+  DependsOn 'lib-alb'
+
+
+
   Parameters do
     ComponentParam 'EnvironmentName', 'dev', isGlobal: true
     ComponentParam 'EnvironmentType', 'development', isGlobal: true, allowedValues: ['development', 'production']
+    ComponentParam 'UserPoolId', ''
+    ComponentParam 'UserPoolClientId', ''
+    ComponentParam 'UserPoolDomainName', ''
+
+    if defined? targetgroup
+      ComponentParam 'DnsDomain'
+      if targetgroup.is_a?(Array)
+        targetgroup.each do |tg|
+          if tg.has_key?('rules')
+            ComponentParam "#{tg['listener']}Listener"
+          else
+            ComponentParam "#{tg['name'].gsub(/[^0-9A-Za-z]/, '')}TargetGroup"
+          end
+        end
+      else
+        ComponentParam 'TargetGroup'
+        ComponentParam 'Listener'
+        ComponentParam 'LoadBalancer'
+      end
+    end
 
     functions.each do |function_name, lambda_config|
       if (lambda_config.has_key? 'enable_eni') && (lambda_config['enable_eni'])
--- lambda.cfndsl.rb
+++ lambda.cfndsl.rb
@@ -137,6 +137,114 @@ CloudFormation do
 
     end if lambda_config.has_key?('events')
 
+
+    service_loadbalancer = []
+    # targetgroups = lambda_config['targetgroup'] if lambda_config.has_key?('targetgroup')
+    # targetgroups = external_parameters.fetch(lambda_config['targetgroup'], {})
+    targetgroups = external_parameters.fetch(:targetgroup, {})
+    
+    # print "aaa"
+
+    print targetgroups
+    rule_names = []
+    unless targetgroups.empty?
+  
+      if !targetgroups.is_a?(Array)
+        # Keep original resource names for backwards compatibility
+        targetgroups['resource_name'] = targetgroup.has_key?('rules') ? 'TaskTargetGroup' : 'TargetGroup'
+        targetgroups['listener_resource'] = 'Listener'
+        targetgroups = [targetgroups]
+      else
+        # Generate resource names based upon the target group name and the listener and suffix with resource type
+        targetgroups.each do |tg| 
+          tg['resource_name'] = "#{tg['name'].gsub(/[^0-9A-Za-z]/, '')}TargetGroup"
+          tg['listener_resource'] = "#{tg['listener']}Listener"
+        end
+      end
+      targetgroups.each do |targetgroup|
+        if targetgroup.has_key?('rules')
+          attributes = []
+    
+          targetgroup['attributes'].each do |key,value|
+            attributes << { Key: key, Value: value }
+          end if targetgroup.has_key?('attributes')
+    
+          tg_tags = tags.map(&:clone)
+    
+          targetgroup['tags'].each do |key,value|
+            tg_tags << { Key: key, Value: value }
+          end if targetgroup.has_key?('tags')
+    
+          ElasticLoadBalancingV2_TargetGroup(targetgroup['resource_name']) do
+            ## Required
+            # Port targetgroup['port']
+            # Protocol targetgroup['protocol'].upcase
+            VpcId Ref('VPCId')
+            ## Optional
+            if targetgroup.has_key?('healthcheck')
+              HealthCheckPort targetgroup['healthcheck']['port'] if targetgroup['healthcheck'].has_key?('port')
+              HealthCheckProtocol targetgroup['healthcheck']['protocol'] if targetgroup['healthcheck'].has_key?('port')
+              HealthCheckIntervalSeconds targetgroup['healthcheck']['interval'] if targetgroup['healthcheck'].has_key?('interval')
+              HealthCheckTimeoutSeconds targetgroup['healthcheck']['timeout'] if targetgroup['healthcheck'].has_key?('timeout')
+              HealthyThresholdCount targetgroup['healthcheck']['healthy_count'] if targetgroup['healthcheck'].has_key?('healthy_count')
+              UnhealthyThresholdCount targetgroup['healthcheck']['unhealthy_count'] if targetgroup['healthcheck'].has_key?('unhealthy_count')
+              HealthCheckPath targetgroup['healthcheck']['path'] if targetgroup['healthcheck'].has_key?('path')
+              Matcher ({ HttpCode: targetgroup['healthcheck']['code'] }) if targetgroup['healthcheck'].has_key?('code')
+            end
+
+            if targetgroup.has_key?('Targets')
+            
+            end
+    
+            TargetType targetgroup['type'] if targetgroup.has_key?('type')
+            TargetGroupAttributes attributes if attributes.any?
+    
+            Tags tg_tags
+          end
+          
+          targetgroup['rules'].each_with_index do |rule, index|
+            listener_conditions = []
+            if rule.key?("path")
+              listener_conditions << { Field: "path-pattern", Values: [ rule["path"] ] }
+            end
+            if rule.key?("host")
+              hosts = []
+              if rule["host"].include?('.') || rule["host"].key?("Fn::Join")
+                hosts << rule["host"]
+              else
+                hosts << FnJoin("", [ rule["host"], ".", Ref("EnvironmentName"), ".", Ref('DnsDomain') ])
+              end
+              listener_conditions << { Field: "host-header", Values: hosts }
+            end
+    
+            if rule.key?("name")
+              rule_name = rule['name']
+            elsif rule['priority'].is_a? Integer
+              rule_name = "TargetRule#{rule['priority']}"
+            else
+              rule_name = "TargetRule#{index}"
+            end
+            rule_names << rule_name
+  
+            actions = [{ Type: "forward", Order: 5000, TargetGroupArn: Ref(targetgroup['resource_name'])}]
+            actions_with_cognito = actions + [cognito(Ref(:UserPoolId), Ref(:UserPoolClientId), Ref(:UserPoolDomainName))]
+            ElasticLoadBalancingV2_ListenerRule(rule_name) do
+              Actions  actions
+              Conditions listener_conditions
+              ListenerArn Ref(targetgroup['listener_resource'])
+              Priority rule['priority']
+            end
+    
+          end
+        end
+    
+        # service_loadbalancer << {
+        #   ContainerName: targetgroup['container'],
+        #   ContainerPort: targetgroup['port'],
+        #   TargetGroupArn: Ref(targetgroup['resource_name'])
+        # }
+      end
+    end
   end if defined? functions
 
 
