6eba70e7ead74c84a32f5ad7239abfd81612aca9
dupontz <gustavo.dupont@gmail.com>
2023-06-16 16:56:23 -0700

    add lb lambda target

--- lambda.cfndsl.rb
+++ lambda.cfndsl.rb
@@ -194,7 +194,8 @@ CloudFormation do
     
             TargetType targetgroup['type'] if targetgroup.has_key?('type')
             TargetGroupAttributes attributes if attributes.any?
-    
+            Targets(FnGetAtt("#{function_name}", 'Arn'))
+
             Tags tg_tags
           end
           
